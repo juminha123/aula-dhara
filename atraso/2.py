69f8a5cd608cfd12199f21b57771e42843018623
juminhadgrau <gjuliasouzagomes@gmail.com>
2026-07-29 12:51:19 -0300

    atividade durante a viagem

---
+++ atraso/2.py
@@ -0,0 +1,12 @@
+a = (float("me infique o primeiro numero"))
+b = (float("me infique o segundo numero"))
+
+soma = a + b
+sub = a - b
+mult = a * b
+div = a / b
+div_inteira = a // b
+resto = a + b
+poten = a ** b
+
+print("os reszultados são:", soma)
---
+++ atraso/4.py
@@ -0,0 +1,6 @@
+ano = float(input(" seu ano de nascimento: "))
+idade = ano - 2026
+if idade >= 18:
+    print("vc é maior")
+else:
+    print("sai fora criança" )    
