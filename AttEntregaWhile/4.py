a40f21d9be936129c19707d97c6ac3a36b3dd394
juminhadgrau <gjuliasouzagomes@gmail.com>
2026-08-03 22:17:33 -0300

    Refactor input loop in 4.py
    Refactor loop to take input until zero is entered and print the counter if greater than the input number.

--- AttEntregaWhile/4.py
+++ AttEntregaWhile/4.py
@@ -1,5 +1,11 @@
 contador = 1
-numero = input("digite um numero: ")
-while contador <= numero:
+numero = 1
+
+
+while contador != 0:
+    contador = int(input("digite um numero: "))
+
+if contador > numero:
+     contador += 1
     print(contador)
-    contador += 1
+   
