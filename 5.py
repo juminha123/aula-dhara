e624d708142bbe4cce61cf0c1452c76156ef17f6
juminhadgrau <gjuliasouzagomes@gmail.com>
2026-08-03 22:31:52 -0300

    Create 5.py
    .

---
+++ 5.py
@@ -0,0 +1,10 @@
+contador = int(input("Digite um número: "))
+numero = 1
+
+
+while contador != 0:
+    contador = int(input("digite um numero: "))
+    
+    if contador < numero:
+        print(contador)
+   
