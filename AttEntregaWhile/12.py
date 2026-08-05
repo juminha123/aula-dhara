f68a2b5323404aaeab9f6588b4a18b71c118a6f7
juminhadgrau <gjuliasouzagomes@gmail.com>
2026-08-05 09:41:44 -0300

    Update 12.py

--- AttEntregaWhile/12.py
+++ AttEntregaWhile/12.py
@@ -1,1 +1,11 @@
-sdtyuiop
+numero = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+resultado = 1
+contador = 0
+while contador < numero2:
+    resultado = resultado * numero
+    contador += 1
+    print(resultado)
+
+
+
